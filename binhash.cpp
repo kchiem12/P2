59d3a3926eb3a302f369acd29801d640336724ff
Daniel Wong <danwong@login13.chn.perlmutter.nersc.gov>
2024-10-09 10:08:56 -0700

    Implemented particle hashing in binhash.cpp

--- binhash.cpp
+++ binhash.cpp
@@ -31,11 +31,42 @@ unsigned particle_bucket(particle_t* p, float h)
 unsigned particle_neighborhood(unsigned* buckets, particle_t* p, float h)
 {
     /* BEGIN TASK */
+    unsigned ix = p->x[0]/h;
+    unsigned iy = p->x[1]/h;
+    unsigned iz = p->x[2]/h;
+    
+    unsigned count = 0;
+
+    for (int dx = -1; dx <= 1; ++dx)
+    {
+        for (int dy = -1; dy <= 1; ++dy)
+        {
+            for (int dz = -1; dz <= 1; ++dz)
+            {
+                unsigned nx = (ix + dx) & HASH_MASK;
+                unsigned ny = (iy + dy) & HASH_MASK;
+                unsigned nz = (iz + dz) & HASH_MASK;
+
+                buckets[count++] = zm_encode(nx, ny, nz);
+            }
+        }
+    }
+
+    return count;
     /* END TASK */
 }
 
 void hash_particles(sim_state_t* s, float h)
 {
     /* BEGIN TASK */
+    memset(s->hash, 0, HASH_SIZE * sizeof(particle_t*));
+
+    for (int i = 0; i < s->n; ++i) {
+        particle_t* p = &s->part[i];
+
+        unsigned bin = particle_bucket(p, h);
+        p->next = s->hash[bin];
+        s->hash[bin] = p;
+    }
     /* END TASK */
 }
